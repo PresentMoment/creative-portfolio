6b99022333db220e83b0511cbe5df2797954b096
Robert Girardin <robertlgirardin@gmail.com>
2019-09-24 08:35:14 +0200

    Updates

---
+++ precache-manifest.52cc2c5c2ed3adf6b2f8c2ee9b7d048f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c89cb42534e67ffe1ae65f50d95017",
+    "url": "/artist/index.html"
+  },
+  {
+    "revision": "bf5b8ac276815f5aac80",
+    "url": "/artist/static/css/main.4634670a.chunk.css"
+  },
+  {
+    "revision": "449730b60b0a05f5231b",
+    "url": "/artist/static/js/2.2cc5c8be.chunk.js"
+  },
+  {
+    "revision": "bf5b8ac276815f5aac80",
+    "url": "/artist/static/js/main.4a127191.chunk.js"
+  },
+  {
+    "revision": "0051b1b8f60949a6ea48",
+    "url": "/artist/static/js/runtime~main.7a3ccbd1.js"
+  }
+]);
